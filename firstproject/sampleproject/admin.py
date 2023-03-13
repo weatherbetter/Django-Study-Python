e57d66628ab862cf94cd46838891c5b291bbfe46
weatherbetter <weatherbetter12@gmail.com>
2023-03-13 11:20:19 +0900

    테이블 생성

--- firstproject/sampleproject/admin.py
+++ firstproject/sampleproject/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from sampleproject.models import Memo
 
 # Register your models here.
+admin.site.register(Memo)
--- firstproject/sampleproject/models.py
+++ firstproject/sampleproject/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Memo(models.Model):
+    memo_text = models.CharField(max_length=250)
+    published_date = models.DateField(auto_now_add=True)
